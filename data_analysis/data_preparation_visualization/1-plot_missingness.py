dafef12057d0244bd9d723a035de4637e2d1fba2
Charlene <charlenee.huynh@gmail.com>
2026-07-12 17:01:29 +0700

    1

--- data_analysis/data_preparation_visualization/1-plot_missingness.py
+++ data_analysis/data_preparation_visualization/1-plot_missingness.py
@@ -6,13 +6,22 @@ import numpy as np
 
 
 def plot_missingness(df):
-    """Visualizes missing values"""
     plt.figure(figsize=(12, 8))
 
-    for position, item in enumerate(df.columns):
-        missing_rows = df[item][df[item].isnull()].index
-        plt.scatter(x=missing_rows, y=[position] * len(missing_rows), marker="|")
-    plt.yticks(range(len(df.columns)), df.columns)
+    for col_idx, col in enumerate(df.columns):
+        missing_rows = np.where(df[col].isnull())[0]
+        plt.scatter(
+            missing_rows,
+            np.full(len(missing_rows), col_idx),
+            marker="|",
+            s=100,
+            color="black",
+        )
 
+    plt.title("Missingness Plot")
+    plt.xlabel("Row Index")
+    plt.yticks(range(len(df.columns)), df.columns)
     plt.tight_layout()
     plt.show()
+
+    return None
