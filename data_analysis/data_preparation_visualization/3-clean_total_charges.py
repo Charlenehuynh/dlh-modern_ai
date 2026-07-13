5f502f019261009033bd56257efddce0b3c888ac
Charlene <charlenee.huynh@gmail.com>
2026-07-13 19:04:19 +0700

    3

---
+++ data_analysis/data_preparation_visualization/3-clean_total_charges.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""Function handles missing values in TotalCharges"""
+
+
+def clean_total_charges(df, method="drop"):
+    df = df.copy()
+    if method == "drop":
+        df = df.dropna(subset=["TotalCharges"])
+    elif method == "median":
+        median_value = df["TotalCharges"].median()
+        df["TotalCharges"] = df["TotalCharges"].fillna(median_value)
+    elif method == "impute":
+        replace = df["MonthlyCharges"] * df["tenure"]
+        df["TotalCharges"] = df["TotalCharges"].fillna(replace)
+    return df
