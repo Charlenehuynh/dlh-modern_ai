c29dfc9031fdd79217989c1ea0d9859566cd1313
Charlene <charlenee.huynh@gmail.com>
2026-07-14 22:38:24 +0700

    9

---
+++ data_analysis/data_preparation_visualization/9-plot_correlation_heatmap.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+"""plot seaborn"""
+
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+
+def plot_correlation_heatmap(df):
+    """
+    df: pandas DataFrame
+    Computes pairwise correlations
+    Generates an annotated heatmap with coolwarm colormap
+    Set vmin = -1 and vmax = 1
+    Displays the plot
+    Returns: None
+    """
+    plt.figure(figsize=(6, 5))
+    corr = df.corr()
+    sns.heatmap(corr, annot=True, fmt="0.2f", cmap="coolwarm", vmin=-1, vmax=1)
+    plt.show()
