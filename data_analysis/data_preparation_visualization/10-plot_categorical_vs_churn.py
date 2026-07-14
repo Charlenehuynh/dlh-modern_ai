5b87e492274f0ca0285143f388e10bd15e8178ad
Charlene <charlenee.huynh@gmail.com>
2026-07-14 23:43:59 +0700

    10

---
+++ data_analysis/data_preparation_visualization/10-plot_categorical_vs_churn.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+import matplotlib.pyplot as plt
+import pandas as pd
+
+
+def plot_categorical_vs_churn(df, col):
+    """
+    df: pandas DataFrame with Churn column
+    col: Categorical column name
+    Uses a figure size of (12, 8)
+    Adds a title to the plot in the format: "Churn Rate by <col>"
+    Plots churn rate (Yes proportion) per category as a bar plot
+    Sets y-axis label to "Churn Rate"
+    Rotates the x-axis labels by 45°
+    Displays the plot
+    Returns: None
+    """
+    churn_rate = (
+        (df["Churn"] == "Yes")
+        .groupby(df[col])
+        .mean()
+        .reset_index(name="Rate")
+    )
+    plt.figure(figsize=(12, 8))
+    plt.bar(churn_rate[col], churn_rate["Rate"])
+    plt.title(f"Churn Rate by {col}")
+    plt.ylabel("Churn Rate")
+    plt.xlabel(col)
+    plt.xticks(rotation=45)
+    plt.show()
