2c554084c71446ca86dbd92db5bb94a9fa74e754
Charlene <charlenee.huynh@gmail.com>
2026-07-15 02:40:50 +0700

    15

---
+++ data_analysis/data_preparation_visualization/15-encode_features.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+"""
+function encode features in the Telco Customer Churn dataset for machine learning modeling.
+"""
+
+import pandas as pd
+from sklearn import preprocessing
+
+
+def encode_features(df):
+    """
+    Encodes features for modeling using Scikit-learn preprocessing tools.
+    """
+    df_enc = df.copy()
+
+    churn_le = preprocessing.LabelEncoder()
+    df_enc["Churn"] = churn_le.fit_transform(df_enc["Churn"])
+
+    binary_cols = ["Partner", "Dependents", "PaperlessBilling", "SeniorCitizen"]
+    binary_oe = preprocessing.OrdinalEncoder()
+    df_enc[binary_cols] = binary_oe.fit_transform(df_enc[binary_cols])
+
+    sorted_tenure_categories = sorted(df["TenureGroup"].unique().tolist())
+    tenure_oe = preprocessing.OrdinalEncoder(categories=[sorted_tenure_categories])
+
+    df_enc["TenureGroup"] = tenure_oe.fit_transform(df_enc[["TenureGroup"]])
+
+    oh_cols = ["Contract", "PaymentMethod"]
+    df_enc = pd.get_dummies(df_enc, columns=oh_cols, drop_first=True, dtype=int)
+
+    return df_enc, churn_le, binary_oe, tenure_oe
