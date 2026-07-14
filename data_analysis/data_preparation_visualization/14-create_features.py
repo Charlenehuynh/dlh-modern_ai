cc7caf9a9bd362640677ee76d8547b444771119b
Charlene <charlenee.huynh@gmail.com>
2026-07-15 02:34:23 +0700

    14

--- data_analysis/data_preparation_visualization/14-create_features.py
+++ data_analysis/data_preparation_visualization/14-create_features.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3
-"""function contains function for new features
-from the Telco Customer Churn dataset."""
+"""
+This module contains the function create_features, which engineers new features
+from the Telco Customer Churn dataset.
+"""
 
 import pandas as pd
 
@@ -32,7 +34,8 @@ def create_features(df):
         else:
             df_copy["NumServices"] += (df_copy[col] == "Yes").astype(int)
 
-    bins = [0, 12, 24, 48, 60, float("inf")]
+    # Change the lowest boundary to -1 to safely include 0 tenure values in the "0-12" group
+    bins = [-1, 12, 24, 48, 60, float("inf")]
     labels = ["0-12", "13-24", "25-48", "49-60", "60+"]
     df_copy["TenureGroup"] = pd.cut(
         df_copy["tenure"], bins=bins, labels=labels, right=True
