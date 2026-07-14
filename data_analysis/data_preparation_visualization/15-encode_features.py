816b49b77cf511639dd34b2f7b2a86fa9203fc1a
Charlene <charlenee.huynh@gmail.com>
2026-07-15 02:49:01 +0700

    15

--- data_analysis/data_preparation_visualization/15-encode_features.py
+++ data_analysis/data_preparation_visualization/15-encode_features.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 """
-function encode features in the Telco Customer Churn dataset for machine learning modeling.
+function encode features in the Telco Customer Churn dataset.
 """
 
 import pandas as pd
@@ -16,15 +16,18 @@ def encode_features(df):
     churn_le = preprocessing.LabelEncoder()
     df_enc["Churn"] = churn_le.fit_transform(df_enc["Churn"])
 
-    binary_cols = ["Partner", "Dependents", "PaperlessBilling", "SeniorCitizen"]
+    binary_cols = (
+        ["Partner", "Dependents", "PaperlessBilling", "SeniorCitizen"])
     binary_oe = preprocessing.OrdinalEncoder(categories=[["No", "Yes"]])
     for col in binary_cols:
         df_enc[col] = binary_oe.fit_transform(df_enc[[col]]).astype(int)
 
     tenure_oe = preprocessing.OrdinalEncoder()
-    df_enc["TenureGroup"] = tenure_oe.fit_transform(df_enc[["TenureGroup"]]).astype(int)
+    df_enc["TenureGroup"] = (tenure_oe.fit_transform
+                             (df_enc[["TenureGroup"]]).astype(int))
 
     oh_cols = ["Contract", "PaymentMethod"]
-    df_enc = pd.get_dummies(df_enc, columns=oh_cols, drop_first=True, dtype=int)
+    df_enc = (pd.get_dummies(df_enc, columns=oh_cols,
+                             drop_first=True, dtype=int))
 
     return df_enc, churn_le, binary_oe, tenure_oe
