77f7bd7f44469c5bf05e3f8122b03e41266d4f0e
Charlene <charlenee.huynh@gmail.com>
2026-07-23 15:42:14 +0200

    0

--- machine_learning/tree_models/0-build.py
+++ machine_learning/tree_models/0-build.py
@@ -7,7 +7,7 @@ from sklearn import tree
 def build_decision_tree(min_samples_leaf, min_samples_split, random_state):
     """ Create a decision tree classifier"""
     n_tree = tree.DecisionTreeClassifier(
-        min_samples_leaf=min_samples_split,
+        min_samples_leaf=min_samples_leaf,
         min_samples_split=min_samples_split,
         random_state=random_state,
     )
