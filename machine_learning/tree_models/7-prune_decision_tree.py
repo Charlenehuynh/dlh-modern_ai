f536116ebf9b8826ec2670b8b2f58c2bc74fefb3
Charlene <charlenee.huynh@gmail.com>
2026-07-24 18:01:35 +0200

    7

---
+++ machine_learning/tree_models/7-prune_decision_tree.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+"""trains multiple decision tree classifiers over a range of parameters"""
+
+from sklearn import tree
+
+train_tree = __import__("1-train").train_tree
+
+
+def prune_and_evaluate_trees(
+    X_train,
+    y_train,
+    X_test,
+    y_test,
+    ccp_alphas,
+    random_state,
+    min_samples_leaf,
+    min_samples_split,
+):
+    """helps analyze how different pruning affect model performance"""
+    clfs = []
+    train_scores = []
+    test_scores = []
+
+    for ccp_alpha in ccp_alphas:
+        clf = tree.DecisionTreeClassifier(
+            random_state=random_state,
+            min_samples_leaf=min_samples_leaf,
+            min_samples_split=min_samples_split,
+            ccp_alpha=ccp_alpha,
+        )
+        train_tree(clf, X_train, y_train)
+        clfs.append(clf)
+        train_scores.append(clf.score(X_train, y_train))
+        test_scores.append(clf.score(X_test, y_test))
+    return clfs, train_scores, test_scores
