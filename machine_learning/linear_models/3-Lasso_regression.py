ebf4c58408fa6bf93c2160df0385cafa267ae39c
Charlene <charlenee.huynh@gmail.com>
2026-07-29 15:54:07 +0200

    3

--- machine_learning/linear_models/3-Lasso_regression.py
+++ machine_learning/linear_models/3-Lasso_regression.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3
 """model creates and returns a Lasso Regression"""
 
+from sklearn import linear_model
+
 
 def lasso_regression(random_state):
     """Creates and returns an untrained Lasso regression model instance."""
