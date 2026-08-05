7fa62856ab81e658ee852212aa9463fc059b7964
Charlene <charlenee.huynh@gmail.com>
2026-08-05 10:28:09 +0200

    1

---
+++ deep_learning/dl_with_keras/1-functional.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+"""Build a shallow neural network using the Functional API."""
+
+from tensorflow import keras
+
+def build_model(input_dim, neurons_h):
+    """
+    Creates a shallow neural network using keras.Model (Functional API).
+
+    Args:
+        input_dim (int): number of input features.
+        neurons_h (int): number of neurons in the hidden layer.
+
+    Returns:
+        model: a Keras Model.
+    """
+    inputs = keras.Input(shape=(input_dim,))
+    hidden = keras.layers.Dense(neurons_h, activation='sigmoid')(inputs)
+    outputs = keras.layers.Dense(10, activation='softmax')(hidden)
+    model = keras.Model(inputs=inputs, outputs=outputs)
+    return model
