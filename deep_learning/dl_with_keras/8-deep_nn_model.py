6627a98eacfda4ecbb82878d84dd8e74a68fdc0f
Charlene <charlenee.huynh@gmail.com>
2026-08-08 15:25:30 +0200

    8

---
+++ deep_learning/dl_with_keras/8-deep_nn_model.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""function to create a deep neural network"""
+
+from tensorflow import keras
+
+
+def build_deep_model(input_dim, hidden_layers):
+    """return Keras mode"""
+    model = keras.Sequential()
+    keras.Input(shape=(input_dim,))
+    for neurons in hidden_layers:
+        model.add(keras.layers.Dense(neurons, activation="relu"))
+    model.add(keras.layers.Dense(10, activation="softmax"))
+    return model
