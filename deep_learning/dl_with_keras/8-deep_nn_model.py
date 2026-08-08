9c25714b88785422ca12a201e184145b3769ca5c
Charlene <charlenee.huynh@gmail.com>
2026-08-08 15:28:01 +0200

    8

--- deep_learning/dl_with_keras/8-deep_nn_model.py
+++ deep_learning/dl_with_keras/8-deep_nn_model.py
@@ -7,7 +7,7 @@ from tensorflow import keras
 def build_deep_model(input_dim, hidden_layers):
     """return Keras mode"""
     model = keras.Sequential()
-    keras.Input(shape=(input_dim,))
+    model.add(keras.Input(shape=(input_dim,)))
     for neurons in hidden_layers:
         model.add(keras.layers.Dense(neurons, activation="relu"))
     model.add(keras.layers.Dense(10, activation="softmax"))
