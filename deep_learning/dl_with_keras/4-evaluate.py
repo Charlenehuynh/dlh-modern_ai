255120d57a23b878c255a7538ce556496e23530f
Charlene <charlenee.huynh@gmail.com>
2026-08-08 11:54:50 +0200

    4

--- deep_learning/dl_with_keras/4-evaluate.py
+++ deep_learning/dl_with_keras/4-evaluate.py
@@ -3,5 +3,9 @@
 
 
 def evaluate_model(model, X, Y, verbose=0):
+    """
+    loss: The calculated loss on the provided data.
+    accuracy: The accuracy of the model on the provided data.
+    """
     loss, accurary = model.evaluate(X, Y, verbose=verbose)
     return loss, accurary
