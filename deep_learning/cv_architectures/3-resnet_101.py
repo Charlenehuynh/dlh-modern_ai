3da6f98a4ef2e7808691a8a16722033d932272b6
Charlene <charlenee.huynh@gmail.com>
2026-08-30 19:39:05 +0200

    3

--- deep_learning/cv_architectures/3-resnet_101.py
+++ deep_learning/cv_architectures/3-resnet_101.py
@@ -1,10 +1,8 @@
 #!/usr/bin/env python3
-"""Builds the ResNet-101 architecture"""
 
 from tensorflow import keras
 
 
 def bottleneck_block(x, filters, stride=1, downsample=False, name=None):
-    """Builds a bottleneck residual block."""
 
     return model
