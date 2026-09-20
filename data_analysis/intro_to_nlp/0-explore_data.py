7653e4770f745342d1ac7ca44ff740af9d60712e
Charlene <charlenee.huynh@gmail.com>
2026-09-20 21:42:35 +0200

    0

--- data_analysis/intro_to_nlp/0-explore_data.py
+++ data_analysis/intro_to_nlp/0-explore_data.py
@@ -2,6 +2,7 @@
 """
 This module performs initial dataset exploration.
 """
+
 import matplotlib.pyplot as plt
 import seaborn as sns
 
@@ -26,16 +27,14 @@ def explore_data(df):
     fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(12, 4))
 
     # Left subplot - bar chart of label distribution
-    label_data = df['label'].value_counts()
+    label_data = df["label"].value_counts()
     sns.barplot(x=label_data.index, y=label_data.values, ax=ax1)
     ax1.set(title="Ham vs Spam Counts", xlabel="label", ylabel="count")
 
     # Right subplot - histogram of message lengths
-    msg_lens = df['message'].str.len()
-    sns.histplot(msg_lens, bins=50, ax=ax2, stat='count', kde=False)
+    msg_lens = df["message"].str.len()
+    sns.histplot(msg_lens, bins=50, ax=ax2, stat="count", kde=False)
     ax2.set(title="Histogram of Raw Message Lengths", xlabel="length",
             ylabel="count")
-
     plt.tight_layout()
     plt.show()
-    
